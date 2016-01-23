36151dd4d51d1fd819ac6b2d2f43c229d133cea9
Orlando Karam <orlando.karam@gmail.com>
2016-01-23 12:41:21 -0500

    script

--- content/scripts/initial-setup.sh
+++ content/scripts/initial-setup.sh
@@ -3,27 +3,26 @@ reponame=$1;
 username=$2;
 password=$3;
 
-if [ "$reponame" == "" ];
+if [ "$reponame" == "" ]
 then
     read -p "Please enter the repository name: " reponame
 fi
 
-if [ "$username" == "" ];
+if [ "$username" == "" ]
 then
     read -p "Please enter the your bitbucket user name: " username
 fi
 
-if [ "$password" == "" ];
+if [ "$password" == "" ]
 then
     read -p "Please enter the your bitbucket password: " password
 fi
 
- 
-if [ -d ~/Documents/projects ];
+if [ -d ~/Documents/projects ]
 then
     echo "projects folder already exists"
 else
-    mkdir ~/Documents/projects
+    mkdir -p ~/Documents/projects
 fi
 
 cd ~/Documents/projects
@@ -37,7 +36,12 @@ else
     if [ -d $reponame ]; then
         echo "cloning successful; downloading .gitignore";
         cd $reponame;
-        curl -o .gitignore 
-
+        curl -o .gitignore "https://raw.githubusercontent.com/okaram/IntroJava/master/content/gitignore_for_projects.txt"
+        if [ -f .gitignore ]; 
+        then
+            echo "gitignore successfully installed";
+        else
+            echo "problem with .gitignore :(";
+        fi
     fi
 fi
